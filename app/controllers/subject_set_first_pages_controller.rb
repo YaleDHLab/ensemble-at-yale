af92381a57d8f1fa276beaba4a628b8322265f15
duhaime <douglas.duhaime@gmail.com>
2016-07-17 10:13:04 -0400

    update subject set first pages controller to use query param for group id to return appropriate first page results

--- app/controllers/subject_set_first_pages_controller.rb
+++ app/controllers/subject_set_first_pages_controller.rb
@@ -2,9 +2,12 @@ class SubjectSetFirstPagesController < ApplicationController
   respond_to :json
 
   def index
-    @all_first_pages = SubjectSetFirstPage.all
+    @requested_group_id = params[:group_id].to_str
+    
+    @first_pages_in_group = SubjectSetFirstPage.where(group_key_id: @requested_group_id).to_a
+
     respond_to do |format|
-      format.json { render json: @all_first_pages }
+      format.json { render json: @first_pages_in_group }
     end
   end
 end
