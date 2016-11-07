7990edfcec797260dc5440ed1e1186315934dbcf
duhaime <douglas.duhaime@gmail.com>
2016-11-07 12:56:55 -0500

    don't display retired subject sets in subject set browsers

--- app/controllers/subject_set_first_pages_controller.rb
+++ app/controllers/subject_set_first_pages_controller.rb
@@ -6,8 +6,18 @@ class SubjectSetFirstPagesController < ApplicationController
     
     @first_pages_in_group = SubjectSetFirstPage.where(group_key_id: @requested_group_id).to_a
 
+    # loop over the first pages in this group and ensure none are retired
+    @active_first_pages_in_group = []
+
+    @first_pages_in_group.each do |o|
+      subject_set = SubjectSet.where("_id" => o.subject_set_id)[0]
+      if subject_set.retired != 1
+        @active_first_pages_in_group << o
+      end
+    end
+
     respond_to do |format|
-      format.json { render json: @first_pages_in_group }
+      format.json { render json: @active_first_pages_in_group }
     end
   end
 end
