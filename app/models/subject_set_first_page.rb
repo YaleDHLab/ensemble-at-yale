8f02bc64bac67e91608b897c8c144ba569014e54
duhaime <douglas.duhaime@gmail.com>
2016-12-08 19:36:46 +0000

    initialize subject set first pages as non-retired

--- app/models/subject_set_first_page.rb
+++ app/models/subject_set_first_page.rb
@@ -5,4 +5,5 @@ class SubjectSetFirstPage
   field :meta_data, type: Hash
   field :group_key_id, type: String
   field :subject_set_id, type: String
+  field :retired, type: Integer, default: 0
 end
